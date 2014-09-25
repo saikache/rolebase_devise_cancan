7045e13945c358383b0c3b29173b893a2e0f2b61
saikache <saisagarkache@gmail.com>
2014-09-25 17:01:06 +0530

    deivise views override

--- config/routes.rb
+++ config/routes.rb
@@ -3,31 +3,6 @@ RolebaseDeviseCancan::Application.routes.draw do
 
   get "home/index"
   root :to => 'home#index'
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
 
   # Sample resource route with sub-resources:
   #   resources :products do
