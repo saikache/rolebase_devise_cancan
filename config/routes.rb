a6a88e66b032ffb17870e6f70094ca88e60497a7
saikache <saisagarkache@gmail.com>
2014-09-26 11:07:04 +0530

    routes devise controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 RolebaseDeviseCancan::Application.routes.draw do
-  devise_for :users
+  devise_for :users , :controllers => {:registrations => "registrations"}
 
   get "home/index"
   root :to => 'home#index'
