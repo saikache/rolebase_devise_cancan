a47356a11ad9d8ba6878d20e9af48da09b0025ad
saikache <saisagarkache@gmail.com>
2014-09-30 15:05:44 +0530

    ability controlling to upload

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,5 @@ class ApplicationController < ActionController::Base
 	rescue_from CanCan::AccessDenied do |exception|
 	  flash[:error] = "Access denied."
 	  redirect_to root_url  
+	end
 end
--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,4 +1,6 @@
 class UploadsController < ApplicationController
+  before_filter :authenticate_user!
+  load_and_authorize_resource
   # GET /uploads
   # GET /uploads.json
   def index
